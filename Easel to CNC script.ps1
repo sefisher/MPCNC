9bc748e34aedb70f2ef5774458dbdceed33ec71b
Stephen Fisher <stephen.e.fisher@gmail.com>
2020-12-20 21:48:43 -0500

    Add files via upload
    Added creation of utility GCODE files for tool change and alignment check.

--- Easel to CNC script.ps1
+++ Easel to CNC script.ps1
@@ -13,7 +13,7 @@
 # on the workpiece (0,0,z-probe-height), 5) Run GCODE, 6) Turn on spindle when paused at safeheight,
 # 7) Click LCD button to continue cutting.
 #
-# What the script does: This script prompts you to open an NC file exported by Easel (usually "Untiled.nc"
+# What the script does: This script prompts you to open an NC file exported by Easel (like "Untiled.nc"
 # in the Downloads folder). Then it confirms the file units are set to mm. It asks the user for a 
 # "safe height" to start. Then it adds a short set of GCODEs to the file's beginning to:
 #    1) turn on absolute positioning, 
@@ -21,6 +21,13 @@
 #    3) raises the bit the safeheight defined below as $defaultSafeHeight or input by the user
 #    4) pauses the machine waiting for an LCD button press at the front panel (to allow turning 
 #       on the spindle and getting it the right speed).
+#
+# It then offers to make 2 other files for workflow:
+#    1) an outline alignment check file that you can run (print) to go around the perimter of the 
+#       the workpiece at the safeheight to check alignment.
+#    2) a tool change file that goes to a tool change position, waits, and then goes to (0,0,Z) to wait for
+#       the user to set the new tool tip a the zprobeoffset height.  Then it resets the coordinates,
+#       goes to the safeheight and ends.
 #
 # NOTE: For most Windows machines you have to run the script bypassing file security policy to allow it 
 #       to open and modify the GCODE file.  To do that:
@@ -31,15 +38,21 @@
 #       -Edit the "Target" field to add "powershell.exe -ExecutionPolicy Bypass -File" in front of the file
 #        (e.g.,  "powershell.exe -ExecutionPolicy Bypass -File D:\3D Objects\CNC Parts\Easel to CNC script.ps1")
 #
+# NOTE: I have a z-probe to zero the machine, but if you manually zero the bit to the top of your 
+#       work piece then just set zProbeOffset to 0
+#
 ################################################################################################
 
-#some parameters
+#some default parameters
 $defaultSafeHeight = 5.000  #mm above Z=0
 $defaultMoveSpeed = 3000    # mm/min for repositioning bit when not cutting
-$firstLine = ";MODIFIED FOR STEVE FISHER'S MPCNC MACHINE USING EASEL - to - MPCNC SCRIPT"
-$Downloads = "D:\Downloads\"
-$SaveFolder = "Q:\"
+$firstLine = ";MODIFIED FOR STEVE FISHER'S MPCNC MACHINE USING EASEL - to - MPCNC SCRIPT" #just a header phrase to recognize modifed files
+$Downloads = "D:\Downloads\" #where to start look for .nc files (dialog will open)
+$SaveFolder = "Q:\" #default where to save new files (dialog will open)
 $zProbeOffset = 0.75  #this should be the thickness of the metal plate used to zero your bit height on top of the work piece
+$xToolChangeDefault = 400 #these are where the MPCNC should go to pause for tool change
+$yToolChangeDefault = 0
+$zToolChangeDefault = 40
 
 Write-Host "`r`n`r`n--Running EASEL to MPCNC--`r`n`r`n"
 
@@ -91,14 +104,132 @@ M00 ; pause for LCD button press
             for($i = 0; $i -lt $numLines; $i++){ 
                 if($lines[$i].StartsWith("G0 ")){
                     $old = $lines[$i]
-                    $lines[$i] = "$old F$defaultMoveSpeed" 
+                    $lines[$i] = "$old F$movespeed" 
                     #Write-Host $lines[$i]    
                 }
             }
 
             #save the file
             Add-Content $FileSave.FileName $lines
-    
+
+            #ask if they want an outline file made (to check alignment of work piece)
+            if (!($outlineQ = Read-Host "`r`nDo you want to create an outline gcode file for checking the alignment of the workpiece? (y/n, default = n)")) {$outlineQ = 'n'}
+            if($outlineQ -eq 'y'){
+                $xQ = Read-Host "`r`nWhat is the X dimension in mm?"
+                $yQ = Read-Host "`r`nWhat is the Y dimension in mm?"
+                if($xQ -and $yQ){
+               
+#Create 0utline file GCODE-----------------------------------------------------------
+$outlineText = 
+@"
+$firstLine
+;To use this outline alignment check file position to the zero starting point for piece 
+;using the Zprobe (or manually to a safe height that is close enough to check alignment).
+;Then run/"print" the file which will move along Y to (0,Ymax), pause (wait for "ok on LCD), 
+;then move along X to (Xmax, Ymax), pause, then along -Y to (Xmax,0), pause, & then to (0,0).
+;NOTE: It moves at the safe height and then lowers to the probeheight (to avoid touching workpiece)
+; - so make sure the bit will clear the workpiece and any clamps at those heights.
+G21 ; Set to metric (mm)
+G90 ; Absolute positioning, just in case
+G92 X0 Y0 Z0 ; Set Current position to 0, all axes
+G92 Z$zProbeOffset ; Account for probe thickness (set your thickness) - this is a coordinate system offset
+G00 Z$safeheight F500 ; Raise Z probe off off of surface
+M00 ; pause for LCD button press
+G00 Y$yQ F$movespeed
+G00 Z$zProbeOffset F500
+M00 ; pause for LCD button press
+G00 Z$safeheight F500
+G00 X$xQ F$movespeed
+G00 Z$zProbeOffset F500
+M00 ; pause for LCD button press
+G00 Z$safeheight F500
+G00 Y0 F$movespeed
+G00 Z$zProbeOffset F500
+M00 ; pause for LCD button press
+G00 Z$safeheight F500
+G00 X0 F$movespeed
+G00 Z$zProbeOffset F500
+"@
+#----------------------------------------------------------------------------------------
+                    
+                    $SaveFolder = Split-Path -Path $newFilename
+                    $savedName = Split-Path -Path $newFilename -Leaf
+                    $newFilename = "CHCK-$savedName"
+                    #open dilog box to save new file
+                    $FileSave = New-Object System.Windows.Forms.SaveFileDialog
+                    $FileSave.InitialDirectory = 
+                    $FileSave.FileName = $newFilename
+
+                    if($FileSave.ShowDialog() -eq 'Ok'){
+
+                        $newFilename = $FileSave.FileName
+        
+                        Write-Host "`r`nSaving the outline alignment check file to: $newFilename."
+        
+                        $newfile = New-Item $FileSave.FileName -type "file" -Value $outlineText -Force
+
+
+                    } else {
+                        Write-Host "`r`n`r`n--Cancelled--"
+                    }
+  
+                }
+            }
+                            #ask if they want a tool change reset file made (to allow getting to a position to change tool, goto (0,0,) , and then re-zero Z)
+                        if (!($toolchangeQ = Read-Host "`r`nDo you want to create a tool change file for keeping X,Y & resetting Z after pause for tool change? (y/n, default = n)")) {$toolchangeQ = 'n'}
+                        if($toolchangeQ -eq 'y'){                            
+                            if (!($xQ = Read-Host "`r`nWhat X position do you want to change the tool (in mm)[default = $xToolChangeDefault]?")) {$xQ = $xToolChangeDefault}
+                            Write-Host "`r`n Got $xQ"
+                            if (!($yQ = Read-Host "`r`nWhat Y position do you want to change the tool (in mm)[default = $yToolChangeDefault]?")) {$yQ = $yToolChangeDefault}
+                            Write-Host "`r`n Got $yQ"
+                            if (!($zQ = Read-Host "`r`nWhat Z position do you want to change the tool (in mm)[default = $zToolChangeDefault]?")) {$zQ = $zToolChangeDefault}
+                            Write-Host "`r`n Got $zQ"
+                            
+               
+#Create TOOL CHANGE file GCODE-----------------------------------------------------------
+$changeText = 
+@"
+$firstLine
+;This tool change file goes to the tool change position then pauses.
+;Once user hits button it goes to (0,0) at the tool change height (z)
+;then it waits for the user to bring the new tool tip to $zProbeOffset
+;using the autohome Z with Zprobe (or manually). Then when the LCD button is pressed 
+;it resets coordinates to (0,0,$zProbeOffset) (in case machine was turned off) and 
+;goes to safe height and ends.
+G21 ; Set to metric (mm)
+G90 ; Absolute positioning, just in case
+G00 Z$zQ F500 ; Raise Z probe off off of surface
+G00 X$xQ Y$yQ F$movespeed
+M00 ; pause for LCD button press
+G00 X0 Y0 F$movespeed
+M00 ; pause for LCD button press
+G92 X0 Y0 Z0 ; Set Current position to 0, all axes
+G92 Z$zProbeOffset ; Account for probe thickness (set your thickness) - this is a coordinate system offset
+G00 Z$safeheight F500
+"@
+#----------------------------------------------------------------------------------------
+                    
+                                $SaveFolder = Split-Path -Path $newFilename
+                                $savedName = Split-Path -Path $newFilename -Leaf
+                                $newFilename = "CHNGE-$savedName"
+                                #open dilog box to save new file
+                                $FileSave = New-Object System.Windows.Forms.SaveFileDialog
+                                $FileSave.InitialDirectory = 
+                                $FileSave.FileName = $newFilename
+
+                                if($FileSave.ShowDialog() -eq 'Ok'){
+
+                                    $newFilename = $FileSave.FileName
+        
+                                    Write-Host "`r`nSaving the tool change file to: $newFilename."
+        
+                                    $newfile = New-Item $FileSave.FileName -type "file" -Value $changeText -Force
+                                } else {
+                                    Write-Host "`r`n`r`n--Cancelled--"
+                                }
+  
+                            
+                        }
         } else {
             Write-Host "`r`n`r`n--Cancelled--"
         }
